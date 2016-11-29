096b2a44d8d9fd1dfb2c587bbf52b24503ddfd8d
fsh <835705572@qq.com>
2016-11-28 23:12:30 -0800

    code

--- code/ccf1.0.py
+++ code/ccf1.0.py
@@ -4,28 +4,29 @@
 import numpy as np
 import pandas as pd
 
+train = pd.read_csv('../data/train.csv', names = ['CONS_NO', 'LABEL'])
+test = pd.read_csv('../data/test.csv', names = ['CONS_NO', 'LABEL'])
 
 reader = pd.read_csv('../data/ALL_USER_YONGDIAN_DATA.zip', compression='zip', iterator=True)
 loop = True
-chunkSize = 10000000
+chunkSize = 8000000
 chunks = []
+num = 1
 
 while loop:
+    if num == 1:
+        header = True
+    else:
+        header = False
     try:
         chunk = reader.get_chunk(chunkSize)
-        chunks.append(chunk)
+        df_train = pd.merge(chunk, train)
+        df_train.to_csv('../data/df_train.csv', index = False, mode='a', header = header)
+        df_test = pd.merge(chunk, test)
+        df_test.to_csv('../data/df_test.csv', index = False, mode = 'a', header = header)
+        num = num + 1
+        del chunk, df_train, df_test
     except StopIteration:
         loop = False
         print "Iteration is stopped."
-data = pd.concat(chunks, ignore_index=True)
-print data.columns
-print 1
-train = pd.read_csv('../data/train.csv')
-test = pd.read_csv('../data/test.csv')
-print 2
-df_train = pd.merge(data, train)
-df_train.to_csv('../data/df_train.csv', index = False)
-del df_train
-print 3
-df_test = pd.merge(data, test)
-df_test.to_csv('../data/df_test.csv', index = False)
+
